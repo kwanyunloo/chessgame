4632c4d8315ba03a34a12f605570f476cb6c88fa
nick-zhang-67 <darkificational@gmail.com>
2026-05-23 19:25:59 -0400

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -8,4 +8,7 @@ public class Piece {
         p = name;
     } 
     //should we insert a move method here? I was thinkg we do it here and hten we override it in all of the classes  
+    public void movePiece(Square s1, Square s2) {
+        
+    }
 }
