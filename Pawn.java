d65e8cedb343720677ab37341dcdadaf6e80808e
nick-zhang-67 <darkificational@gmail.com>
2026-05-27 14:29:50 -0400

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -1,18 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chess;
 
-/**
- *
- * @author 4004960
- */
 public class Pawn extends Piece {
+
+    private boolean hasMoved;
     
     public Pawn(boolean newColor) {
         super(newColor);
     }
 
     public ArrayList<int[]> possibleMoves(Square current, Square[][] board)
+    {
+        int[][] directions;
+
+        if (current.getPiece().getColor()) {
+            directions = {
+                {0, 1},
+                {0, 2},
+                {1, 1},
+                {-1, 1}
+            };
+        } else {
+            directions = {
+                {0, -1},
+                {0, -2},
+                {1, -1},
+                {-1, -1}
+            };
+        }
+    }
 }
