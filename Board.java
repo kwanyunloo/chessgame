dc1832e963ea4cd59c852e79135526c780cab1fb
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-27 11:07:10 -0400

    Refactor checkIfKingInCheck method and fix logic

--- Board.java
+++ Board.java
@@ -20,14 +20,30 @@ public class Board {
             board[6][i].addPiece((Piece) (new Pawn(p2)));
         }
     }
-    public boolean checkIfKingInCheck(){
+    public boolean checkIfKingInCheck(boolean color){
+        int kingRowOfColor = -1;
+        int kingColOfColor = -1;
         ArrayList<int[]> placesOpposingPlayerCovers = new ArrayList<>();
         for (int i = 0; i < board.length; i++){
             for (int j = 0; j < board[0].length; j++){
                 if (!board[i][j].empty){
-                    int[] toAdd = new int[]
+                    int[] toAdd = new int[2];
+                    toAdd[0] = i;
+                    toAdd[1] = j;
+                    if (board[i][j].getPiece().getColor()){
+                        kingRowOfColor = i;
+                        kingRowOfColor = j;
+                    }
+                    placesOpposingPlayerCovers.add(toAdd);
                 }
             }
         }
+        for (int[] arr : placesOpposingPlayerCovers){
+            if (arr[0] == kingRowOfColor && arr[1] == kingColOfColor){
+                return true;
+            }
+        }
+        return false;
+        
     }
 }
