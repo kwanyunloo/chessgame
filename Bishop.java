e14a473dfda45bb2562a0ac456a8af86f59bd603
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-27 10:50:39 -0400

    Update Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -13,5 +13,47 @@ public class Bishop extends Piece {
     public Bishop(Player name) {
         super(name);
     }
+
+    public ArrayList<int[]> move(Square current, Square[][] board) {
+
+        ArrayList<int[]> moves = new ArrayList<>();
+
+        int[][] directions = {
+            {-1, -1},
+            {-1, 1},
+            {1, -1},
+            {1, 1}
+        };
+
+        for (int[] d : directions) {
+
+            int newX = current.x + d[0];
+            int newY = current.y + d[1];
+
+            while (newX >= 0 && newX < 8 &&
+                   newY >= 0 && newY < 8) {
+
+                if (board[newX][newY].piece == null) {
+
+                    moves.add(new int[]{newX, newY});
+                }
+
+                else if (board[newX][newY].piece.owner != this.owner) {
+
+                    moves.add(new int[]{newX, newY});
+                    break;
+                }
+
+                else {
+                    break;
+                }
+
+                newX += d[0];
+                newY += d[1];
+            }
+        }
+
+        return moves;
+    }
     
 }
