eceaafebb5fb9820428f13546a1de1ee776fb1c7
TheTop-P <prachetpadhy@gmail.com>
2026-05-27 14:11:37 -0400

    Update driver.java

--- driver.java
+++ driver.java
@@ -2,5 +2,15 @@ public class driver{
 
     public static void main(String[] args){
         System.out.println("Hello");
+           board[3][3].addPiece(rook);
+
+        // get possible moves
+        ArrayList<int[]> moves =
+                rook.possibleMoves(board[3][3], board);
+
+        // print moves
+        for (int[] move : moves) {
+            System.out.println("(" + move[0] + ", " + move[1] + ")");
+        }
     }
 }
