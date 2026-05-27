431a9bf1fd66648c92c95c5466a9b8f8ad66e336
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-27 10:42:46 -0400

    Update King.java

--- King.java
+++ King.java
@@ -13,33 +13,43 @@ public class King extends Piece {
     public King(Player name) {
         super(name);
     }
-    public ArrayList<int[]> possibleMoves(Square current, Square[][] board){
-        ArrayList<int[]> toReturn = new ArrayList<>();
-        if (current.x + 1 < 8){
-            int[] toAdd = new int[2];
-            toAdd[0] = current.x+1;
-            toAdd[1] = current.y;
-            toReturn.add(toAdd);
+    public ArrayList<int[]> move(Square current, Square[][] board) {
+
+        ArrayList<int[]> moves = new ArrayList<>();
+
+        int[][] directions = {
+            {-1, -1},
+            {-1, 0},
+            {-1, 1},
+            {0, -1},
+            {0, 1},
+            {1, -1},
+            {1, 0},
+            {1, 1}
+        };
+
+        for (int[] d : directions) {
+
+            int newX = current.x + d[0];
+            int newY = current.y + d[1];
+
+            if (newX >= 0 && newX < 8 &&
+                newY >= 0 && newY < 8) {
+
+                if (board[newX][newY].piece == null) {
+
+                    moves.add(new int[]{newX, newY});
+                }
+
+                else if (board[newX][newY].piece.owner != this.owner) {
+
+                    moves.add(new int[]{newX, newY});
+                }
+            }
         }
-        if (current.x - 1 >= 0){
-            int[] toAdd = new int[2];
-            toAdd[0] = current.x+1;
-            toAdd[1] = current.y;
-            toReturn.add(toAdd);
-        }
-        if (current.y - 1 >= 0){
-            int[] toAdd = new int[2];
-            toAdd[0] = current.x;
-            toAdd[1] = current.y-1;
-            toReturn.add(toAdd);
-        }
-        if (current.y + 1 < 8){
-            int[] toAdd = new int[2];
-            toAdd[0] = current.x;
-            toAdd[1] = current.y+1;
-            toReturn.add(toAdd);
-        }
-        return toReturn;
+
+        return moves;
+    }
     }
     
 }
