bac1738f303c79d24546ec4bffeb100c91b3b959
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-27 14:25:27 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -15,7 +15,7 @@ public class Rook extends Piece {
         super(newColor);
     }
       public ArrayList<int[]> possibleMoves(Square current, Square[][] board){
-        ArrayList<int[]> possibleMoves = new ArrayList<>();
+        ArrayList<int[]> moves = new ArrayList<>();
         int row = current.getRow();
         int col = current.getCol();
   //4 directions in which a rook can move: up, down, left, and right
@@ -24,26 +24,27 @@ public class Rook extends Piece {
             {1,0},    //down
             {0,-1},   //left
             {0,1},    //right
-        }
+        };
         for(int [] d: directions){
                  int newRow = row + d[0];
                 int newCol = col + d[1];
   //the purpose of the while statement is to keep moving until the edge or you are blocked
-                while(newRow>=0&& newRow<8 && newCol>=0 && newCol<=8){
+                while(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol< 8){
                     Square next=board[newRow][newCol]; //gets square on the chessboard at a specific position and stores it 
                     if(!next.hasPiece()){
-                        moves.add(new int[]{newRow,newCol};
-                    }else{
+                        moves.add(new int[]{newRow,newCol});
+                    }
+                    else{
                         // if enemy piece, you can capture
                         
-                        if(next.getPiece()getColor()!= this.getColor()){
-                            moves.add(new int[]{newRow, newCol}); //record coordinate as legal move for this piece
+                        if (next.getPiece().getColor() != this.getColor()){
+                            moves.add(new int[]{newRow, newCol}); // record coordinate as legal move for this piece
                         }
                         //stop scanning in this direction
                         break;
                 
             }    
-                    newRow += d[o];
+                    newRow += d[0];
                     newCol += d[1];
       }
     }
