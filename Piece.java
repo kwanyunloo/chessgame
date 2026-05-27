d1a6cd255367102ff5f838bf9669f0141499edf3
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-27 13:56:37 -0400

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -5,28 +5,10 @@ public class Piece {
     Player p;
     boolean isWhite;
 
-    //should we insert a move method here? I was thinkg we do it here and hten we override it in all of the classes  
     abstract public void movePiece(Square s1, Square s2);
-    //see if i did it right
+    
     public boolean getColor(){
         return color;
     }
-    private boolean checkPin(int diagonalsChecked, int rowsChecked)
-    {   
-        int diagonals = diagonalsChecked;
-        int rows = rowsChecked;
-        if (diagonalsChecked == 2 && rowsChecked == 2)
-        {
-            return false;
-        } else {
-            if (diagonals < 2)
-            {
-                if (diagonals == 0)
-                {
-                    while (Square s1
-                    
-            }
-            checkPin(diagonals, rows);
-        }
-    }
+    
 }
