fc4afc79eba8b40441033adfce4ac6a93afbe2c0
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-27 14:02:00 -0400

    Add possibleMoves method to Queen class
    Implemented possibleMoves method for Queen class to calculate valid moves on the chessboard.

--- Queen.java
+++ Queen.java
@@ -13,5 +13,64 @@ public class Queen extends Piece {
     public Queen(Player name) {
         super(name);
     }
+    public ArrayList<int[]> possibleMoves(Square square, Square[][] board){
+        ArrayList<int[]> moves = new ArrayList<>();
+        int originalRow = square.getRow();
+        int originalCol = square.getCol();
+        // left moving 
+        for (int col = originalCol; i >= 0; i--){
+            if (board[originalRow][col].hasPiece()){
+                break;
+            }else{
+                int[] toAdd = new int[2];
+                toAdd[0] = originalRow;
+                toAdd[1] = col;
+                moves.add(toAdd);
+            }
+        }
+        // right moving
+        for (int col = originalCol; i < 8; i++){
+            if (board[originalRow][col].hasPiece()){
+                break;
+            }else{
+                int[] toAdd = new int[2];
+                toAdd[0] = originalRow;
+                toAdd[1] = col;
+                moves.add(toAdd);
+            }
+        }
+
+        // above
+        for (int row = originalRow; i >= 0; i--){
+            if (board[row][col].hasPiece()){
+                break;
+            }else{
+                int[] toAdd = new int[2];
+                toAdd[0] = row;
+                toAdd[1] = originalCol;
+                moves.add(toAdd);
+            }
+        }
+        // below
+        for (int row = originalRow; i < 8; i++){
+            if (board[row][col].hasPiece()){
+                break;
+            }else{
+                int[] toAdd = new int[2];
+                toAdd[0] = row;
+                toAdd[1] = originalCol;
+                moves.add(toAdd);
+            }
+        }
+        // upper right diagnal
+        for (int row = originalRow; i < 8; i++){
+            
+        }
+        // upper left diagnal
+
+        // lower right diagnal
+
+        // lower left diagnal
+    }
     
 }
