3c73ae2b5237f588f4cf6d4f2f0be5c8c362cdaa
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-27 14:29:26 -0400

    Refactor Board initialization and add move method

--- Board.java
+++ Board.java
@@ -3,23 +3,44 @@ package chess;
 public class Board {
     
     private Square[][] board = new Square[8][8];
-    
-    // Initializes the starting board position
+    // TODO: Make moves
+    public void move(int startRow, int startCol, int endRow, int endCol){
+        // need to check what piece is at start row start col and if it can move to the end row end col or not
+        // If not, then send a message to the user that this move is illegal
+    }
     public Board(){
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
+         for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
                 board[i][j] = new Square(i, j);
             }
         }
-        
-        for (int i = 0; i < 8; i++){
-            
-            board[1][i].addPiece((Piece) (new Pawn(true)));
-            board[6][i].addPiece((Piece) (new Pawn(false)));
+        board[0][0].addPiece(new Rook(true));
+        board[0][1].addPiece(new Knight(true));
+        board[0][2].addPiece(new Bishop(true));
+        board[0][3].addPiece(new Queen(true));
+        board[0][4].addPiece(new King(true));
+        board[0][5].addPiece(new Bishop(true));
+        board[0][6].addPiece(new Knight(true));
+        board[0][7].addPiece(new Rook(true));
+
+        for (int i = 0; i < 8; i++) {
+            board[1][i].addPiece(new Pawn(true));
+        }
+
+        board[7][0].addPiece(new Rook(false));
+        board[7][1].addPiece(new Knight(false));
+        board[7][2].addPiece(new Bishop(false));
+        board[7][3].addPiece(new Queen(false));
+        board[7][4].addPiece(new King(false));
+        board[7][5].addPiece(new Bishop(false));
+        board[7][6].addPiece(new Knight(false));
+        board[7][7].addPiece(new Rook(false));
+
+        for (int i = 0; i < 8; i++) {
+            board[6][i].addPiece(new Pawn(false));
         }
     }
+    
     public boolean checkIfKingInCheck(boolean color){
         int kingRowOfColor = -1;
         int kingColOfColor = -1;
