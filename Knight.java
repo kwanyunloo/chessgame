4319b0fe51380384321a6cea88c7a35d5c691e40
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-27 10:55:15 -0400

    Implement possibleMoves method for Knight class
    The possibleMoves method calculates valid moves for the Knight piece based on its current position on the chessboard.

--- Knight.java
+++ Knight.java
@@ -4,6 +4,8 @@
  */
 package chess;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author 4004960
@@ -12,6 +14,51 @@ public class Knight extends Piece {
     
     public Knight(Player name) {
         super(name);
+
+    }
+    public ArrayList<int[]> possibleMoves(Square current){
+        ArrayList<int[]> possibleMoves = new ArrayList<>();
+        int row = current.getRow();
+        int col = current.getCol();
+        if (row + 2 < 8 && col + 1 < 8){
+            int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd);
+        }
+        if (row + 1 < 8 && col + 2 < 8){
+            int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd);
+        }
+        if (row - 2 < 8 && col - 1 < 8){
+            int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd);
+        }
+        if (row - 1 < 8 && col - 2 < 8){
+            int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd);
+        }
+        if (row - 2 < 8 && col + 1 < 8){
+            int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd);
+        }
+        if (row + 1 < 8 && col - 2 < 8){
+           int[] toAdd = new int[2];
+            toAdd[0] = row;
+            toAdd[1] = col;
+            possibleMoves.add(toAdd); 
+        }
+
+
+        return possibleMoves;
     }
     
 }
