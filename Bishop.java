57c7020138e7e0293a0267c1d0fa6329ac1ec280
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-27 14:23:48 -0400

    Edit bishop class

--- Bishop.java
+++ Bishop.java
@@ -12,14 +12,13 @@ import java.util.ArrayList;
 
 public class Bishop extends Piece {
     
-    public Bishop(boolean newColor) {
-        super(newColor);
+    public Bishop(Player name) {
+        super(name);
     }
 
     public ArrayList<int[]> possibleMoves(Square current, Square[][] board) {
-
-        ArrayList<int[]> moves = new ArrayList<>();
-
+        ArrayList<int[]> possibleMoves = new ArrayList<>();
+        boolean currentColor = current.getPiece().getColor();
         int[][] directions = {
             {-1, -1},
             {-1, 1},
@@ -33,14 +32,25 @@ public class Bishop extends Piece {
             int newCol = current.getCol() + d[1];
 
             while (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
-
+                int[] toAdd = new int[2];
+                toAdd[0] = newRow;
+                toAdd[1] = newCol;
                 Square next = board[newRow][newCol];
-
+                if (next.hasPiece()){
+                    if (next.getPiece().getColor() == currentColor){
+                        possibleMoves.add(toAdd);
+                        break;
+                    }else{
+                        break;
+                    }
+                }
+                
                 
 
                 newRow += d[0];
                 newCol += d[1];
             }
         }
+        return possibleMoves;
     
 }
