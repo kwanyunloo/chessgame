8db71abb0ad510ced513ad1d8421521b78bf3033
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-29 09:57:30 -0400

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chess;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author 4004960
- */
 public class Knight extends Piece {
     
     public Knight(boolean newColor) {
