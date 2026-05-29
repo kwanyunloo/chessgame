607cc8b3601622f297e408db2235ad779cecddd3
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-29 09:58:59 -0400

    Update Chess class with game loop and input handling
    Refactored Chess class to include a game loop and scanner for user input.

--- Chess.java
+++ Chess.java
@@ -1,19 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package chess;
 
+/**
+ *
+ * @author 4004960
+ */
 // This acts as the driver class for our program
 public class Chess{
 
     public static void main(String[] args){
-        System.out.println("Hello");
-           board[3][3].addPiece(rook);
+        Scanner in = new Scanner(System.in);
+        Board board = new Board();
+        while (!board.isGameOver()){
 
-        // get possible moves
-        ArrayList<int[]> moves =
-                rook.possibleMoves(board[3][3], board);
-
-        // print moves
-        for (int[] move : moves) {
-            System.out.println("(" + move[0] + ", " + move[1] + ")");
         }
     }
 }
