7854c9dc6f4489f61d6a5ba89995160c0e4a6995
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-29 09:56:52 -0400

    Update Rook.java

--- Rook.java
+++ Rook.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chess;
 
-/**
- *
- * @author 3017317
- */
 import java.util.ArrayList;
+
 public class Rook extends Piece {
     boolean hasMoved();
     
