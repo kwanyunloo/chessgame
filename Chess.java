79d627ec3333575e4ad8c680c41fee4ffe9d5ef0
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-29 10:34:53 -0400

    Enhance Chess main method with player prompts
    Added player turn prompts and move handling in main method.

--- Chess.java
+++ Chess.java
@@ -12,10 +12,22 @@ package chess;
 public class Chess{
 
     public static void main(String[] args){
+        
         Scanner in = new Scanner(System.in);
         Board board = new Board();
+        
         while (!board.isGameOver()){
-
+            if (board.playerTurn()){
+                System.out.println("White to move. Enter the starting square and ending square.");
+            }else{
+                System.out.println("Black to move. Enter the starting square and ending square.");
+            }
+            int startingRow = in.nextInt();
+            int startingCol = in.nextInt();
+            int endingRow = in.nextInt();
+            int endingCol = in.nextInt();
+            board.move(startinRow, startingCol, endingRow, endingCol);
+            System.out.println(board); // make sure to make a toString method that prints it out correctly (rotate it)
         }
     }
 }
