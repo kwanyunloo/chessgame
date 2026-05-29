135e0cbbd3ec2be16e98a10fabe94402030f8e33
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-29 11:00:40 -0400

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -5,29 +5,69 @@ import java.util.ArrayList;
 public class Pawn extends Piece {
 
     private boolean hasMoved;
+    private boolean justMovedTwo;
     
     public Pawn(boolean newColor) {
         super(newColor);
     }
+
     @Override
     public ArrayList<int[]> possibleMoves(Square current, Square[][] board)
     {
-        int[][] directions;
+        ArrayList<int[]> moves = new ArrayList<>();
+        int row = current.getRow();
+        int col = current.getCol();
 
+        int direction;
         if (current.getPiece().getColor()) {
-            directions = {
-                {0, 1},
-                {0, 2},
-                {1, 1},
-                {-1, 1}
-            };
-        } else {
-            directions = {
-                {0, -1},
-                {0, -2},
-                {1, -1},
-                {-1, -1}
-            };
+            direction = 1;
+        } 
+        else {
+            direction = -1;
         }
+
+        int nextRow = row + direction;
+        if (nextRow >= 0 && nextRow < 8){
+            //forward 1 square
+            Square forwardSquare = board[nextRow][col]
+            if (!forwardSquare.hasPiece()){
+                moves.add(new int[]{nextRow, col});
+            }
+
+            //forward 2 squares
+            int doubleRow = row + (2 * direction);
+                if (!hasMoved && doubleRow >= 0 && doubleRow < 8) {
+                    Square doubleForwardSquare = board[doubleRow][col];
+                    if (!doubleForwardSquare.hasPiece()) {
+                        moves.add(new int[]{doubleRow, col});
+                        justMovedTwo = true;
+                    }
+                }
+        }
+
+        //diagonal capturing
+        int col1 = col + 1;
+        int col2 = col - 1;
+        if (nextRow >= 0 && nextRow < 8) {
+                if (col1 >= 0 && col1 < 8){
+                    Square targetSquare = board[nextRow][col1];
+                    if (targetSquare.hasPiece() && targetSquare.getPiece().getColor() != this.getColor()) {
+                        moves.add(new int[]{nextRow, nextCol});
+                    }
+                }
+                if (col1 >= 0 && col1 < 8){
+                    Square targetSquare = board[nextRow][col2];
+                    if (targetSquare.hasPiece() && targetSquare.getPiece().getColor() != this.getColor()) {
+                        moves.add(new int[]{nextRow, nextCol});
+                    }
+                }
+            }
+    }
+
+    public void setHasMoved(boolean b) {
+        hasMoved = b;
+    }
+    public void setJustMovedTwo(boolean b){
+        justMovedTwo = b;
     }
 }
