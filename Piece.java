457a13a649d68c717c2b1d2e68990986388f154e
kwanyunloo <lkwanyunloo@gmail.com>
2026-05-31 10:00:56 -0400

    Enhance Piece class with movement tracking
    Added hasMoved property and its getter/setter methods.

--- Piece.java
+++ Piece.java
@@ -1,10 +1,11 @@
 package chess;
-
+//used AI to fix small errors
 import java.util.ArrayList;
 
 public abstract class Piece {
     // True for White, False for Black
     protected boolean color;
+    protected boolean hasMoved = false;
 
     public Piece(boolean newColor) {
         color = newColor;
@@ -12,6 +13,14 @@ public abstract class Piece {
 
     public boolean getColor() {
         return this.color;
+    }
+
+    public boolean hasMoved() {
+        return hasMoved;
+    }
+
+    public void setHasMoved(boolean b) {
+        hasMoved = b;
     }
 
     public abstract ArrayList<int[]> possibleMoves(Square current, Square[][] board);
