1a5cf55f355037d9dfd96b0c2da67357a5e86b39
brianshangguan-maker <brianshangguan@gmail.com>
2026-05-31 22:09:58 -0400

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -2,19 +2,28 @@
 import java.util.ArrayList;
 
 public abstract class Piece {
+    
     // True for White, False for Black
     protected boolean color;
 
     //used for castling and pawn moves
     protected boolean hasMoved;
 
-    //returns the color of the piece
     public Piece(boolean newColor) {
         color = newColor;
     }
-
+    
+    //returns the color of the piece
     public boolean getColor() {
         return this.color;
+    }
+
+    public boolean getHasMoved() {
+        return hasMoved;
+    }
+
+    public void setHasMoved(boolean b) {
+        hasMoved = b;
     }
 
     public abstract ArrayList<int[]> possibleMoves(Square current, Square[][] board);
